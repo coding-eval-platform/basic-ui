2dc54f8b53ffa7d59460a36b028aed2e061b0d92
Daniel Lobo <dlobo@itba.edu.ar>
2019-08-04 19:31:33 -0300

    change time

--- components/Teacher/ExamRow.js
+++ components/Teacher/ExamRow.js
@@ -39,7 +39,9 @@ const convertHMS = (value) => {
   if (hours   < 10) {hours   = "0"+hours;}
   if (minutes < 10) {minutes = "0"+minutes;}
   if (seconds < 10) {seconds = "0"+seconds;}
-  return hours+':'+minutes+':'+seconds; // Return is HH : MM : SS
+  // return hours+':'+minutes+':'+seconds; // Return is HH : MM : SS
+  // return hours+'days '+minutes+'hs '+seconds + 'mins'; 
+  return minutes+'hs '+seconds + 'mins'; 
 }
 
 
