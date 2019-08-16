826732b2c5a3f6d5d9b9f48e8c7fc5bcce87d302
Daniel Lobo <dlobo@itba.edu.ar>
2019-08-16 15:31:14 -0300

    Improve homepage

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,35 @@
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
+import { Button } from '@material-ui/core'
 import Layout from '../components/MyLayout.js'
-import RubyExercise from '../components/Editors/RubyExercise.js'
+import Typography from '@material-ui/core/Typography'
 
 export default () => {
   return (
     <Layout>
-      
-      {/*<Link href="/about">
-        <button>Go to About Page</button>
-      </Link> 
-      */}
-
-      <RubyExercise />
+      <Typography style={{ margin: 8 }} variant="h5" gutterBottom>
+        Welcome to the Coding Evaluation Platform 🚀
+      </Typography>
+      <Typography style={{ margin: 8 }} variant="h5" gutterBottom>
+        New user?
+        <Button
+          variant="outlined"
+          color="primary"
+          href="/signup"
+          style={{ textTransform: 'none', margin: 20 }}
+        >
+          Sign up here
+        </Button>
+      </Typography>
+      <Typography style={{ margin: 8 }} variant="h5" gutterBottom>
+        Existing user?
+        <Button
+          variant="outlined"
+          color="primary"
+          href="/login"
+          style={{ textTransform: 'none', margin: 20 }}
+        >
+          Login here
+        </Button>
+      </Typography>
     </Layout>
   )
 }
-
